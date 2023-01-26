2f2ce71d32ef57c77bec5b6da805657acbe6fab3
sastmyhero <stasmyhero@gmail.com>
2023-01-26 23:53:59 +0300

    calculator

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import '~/js/components/burger';
 import '~/js/components/dropdown';
 import '~/js/components/collapse';
 import '~/js/components/modal';
+import '~/js/components/calculator';
--- src/js/components/calculator.js
+++ src/js/components/calculator.js
@@ -1,51 +1,48 @@
-const { debounce } = require('../utils/common');
-const { watchData } = require('../utils/watch');
+import { debounce } from '../utils/common';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const sumDiv = document.querySelector('calculator__sum-single');
-  const sumPerMonthDiv = document.querySelector('calculator__sum-multiple');
+  const sumDivs = document.querySelectorAll('.calculator__sum');
+  const sumPerMonthDivs = document.querySelectorAll(
+    '.calculator__sum-per-month'
+  );
 
-  const oneTimeSum = 1990;
-  const sumPerMonth = 0;
+  let commonPrice = 1990;
+  let commonMonthlyPrice = 0;
 
   const getSum = () => {
-    return oneTimeSum;
+    return commonPrice;
   };
 
   const setSum = (value) => {
-    sumDiv.dataset.value = value;
+    commonPrice = parseInt(value, 10);
+    sumDivs.forEach((sumDiv) => {
+      sumDiv.textContent = commonPrice;
+    });
   };
 
   const getSumPerMonth = () => {
-    return sumPerMonth;
+    return commonMonthlyPrice;
   };
 
   const setSumPerMonth = (value) => {
-    sumPerMonthDiv.dataset.value = value;
+    commonMonthlyPrice = parseInt(value, 10);
+
+    sumPerMonthDivs.forEach(
+      (sumPerMonthDiv) => (sumPerMonthDiv.textContent = commonMonthlyPrice)
+    );
   };
 
-  const renderResult = () => {
-    sumDiv.innerText = sumDiv;
-    sumPerMonthDiv.innerText = sumPerMonth;
-  };
-
-  const addSum = (sum, perMonth, multiplier = 1) => {
+  const addSums = (sum, perMonth, multiplier = 1) => {
     setSum(getSum() + sum * multiplier);
     setSumPerMonth(getSumPerMonth() + perMonth * multiplier);
   };
 
-  const subSum = (sum, perMonth, multiplier = 1) => {
-    setSum(getSum() + sum * multiplier);
-    setSumPerMonth(getSumPerMonth() + perMonth * multiplier);
+  const subSums = (sum, perMonth, multiplier = 1) => {
+    setSum(getSum() - sum * (multiplier == 0 ? 1 : multiplier));
+    setSumPerMonth(
+      getSumPerMonth() - perMonth * (multiplier == 0 ? 1 : multiplier)
+    );
   };
-
-  watchData(sumDiv, 'value', () => {
-    renderResult();
-  });
-
-  watchData(sumPerMonthDiv, 'value', () => {
-    renderResult();
-  });
 
   const getInputValue = (input) => {
     return parseInt(input.value, 10);
@@ -53,29 +50,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const setInputValue = (input, value) => {
     input.value = String(value);
+    input.dispatchEvent(new Event('change'));
   };
 
   const increment = (input) => {
-    setInputValue(getInputValue(input) + 1);
-    input.dispatchEvent(new Event('change'));
+    input.prevValue = getInputValue(input);
+    setInputValue(input, getInputValue(input) + 1);
   };
 
   const decrement = (input) => {
-    setInputValue(getInputValue(input) - 1);
+    const value = getInputValue(input);
+    if (value <= 0) {
+      return;
+    }
+    input.prevValue = value;
+    setInputValue(input, getInputValue(input) - 1);
     input.dispatchEvent(new Event('change'));
   };
-
-  const onChange = function (input, prev) {
-    if (getInputValue(input) > prev) {
-      addSum(fixed, getInputValue(input));
-      addSumPerMonth(perMonth, getInputValue(input));
-    } else {
-      subSum(fixed, getInputValue(input));
-      subSumPerMonth(perMonth, getInputValue(input));
-    }
-  };
-
-  const onChangeDebounced = debounce(onChange);
 
   const allowedKeys = [
     'Enter',
@@ -98,27 +89,98 @@ document.addEventListener('DOMContentLoaded', () => {
     '9'
   ];
 
-  document.querySelectorAll('calcalator__multiplier').forEach((multiplier) => {
-    const input = multiplier.querySelector('calcaultor__input');
-    const fixed = input.dataset.fixed;
-    const perMonth = input.dataset.per;
+  const updatePrices = (inputValue, input, price, monthlyPrice) => {
+    const priceParentDiv =
+      input?.parentElement?.parentElement.querySelector('.calculator__price');
+
+    const priceDiv = priceParentDiv?.querySelector('.calculator__price-fixed');
+    const monthlyPriceDiv = priceParentDiv.querySelector(
+      '.calculator__price-per-month'
+    );
+    const tooltip = priceParentDiv.querySelector('.tooltip');
+
+    if (inputValue > 0) {
+      priceDiv.textContent = `${price * inputValue} ₽ разово${
+        monthlyPriceDiv ? ',' : ''
+      }`;
+      priceDiv.classList.remove('calculator__price_disabled');
+      if (monthlyPriceDiv && monthlyPrice) {
+        monthlyPriceDiv.textContent = `${monthlyPrice} ₽ / месяц.`;
+      }
+      if (tooltip) {
+        tooltip.style.display = 'block';
+      }
+    } else {
+      priceDiv.textContent = 'Не выбрано';
+      priceDiv.classList.add('calculator__price_disabled');
+      if (tooltip) {
+        tooltip.style.display = 'none';
+      }
+      if (monthlyPriceDiv && monthlyPrice) {
+        monthlyPriceDiv.textContent = '';
+      }
+    }
+  };
+
+  const onChange = function (input, price, monthlyPrice) {
+    const inputValue = getInputValue(input);
+    const prevValue = parseInt(input.prevValue, 10);
+    if (inputValue > prevValue) {
+      addSums(price, monthlyPrice, inputValue);
+    } else {
+      subSums(price, monthlyPrice, inputValue);
+    }
+    updatePrices(inputValue, input, price, monthlyPrice);
+  };
+
+  const onChangeDebounced = debounce(onChange, 100);
+
+  document.querySelectorAll('.calculator__multiplier').forEach((multiplier) => {
+    const input = multiplier.querySelector('.calculator__input');
+    const price = multiplier.dataset.price;
+    const monthlyPrice = multiplier.dataset.monthlyPrice;
+
+    const handleInputChange = (event) => {
+      if (event.target.value === '') {
+        event.target.value = 0;
+      }
+      onChangeDebounced(input, price, monthlyPrice);
+    };
 
     multiplier
-      .querySelector('calculator__increment')
-      .addEventListener('click', increment(input));
+      .querySelector('.calculator__increment')
+      .addEventListener('mousedown', () => increment(input));
     multiplier
-      .querySelector('calculator__decrement')
-      .addEventListener('click', decrement(input));
+      .querySelector('.calculator__decrement')
+      .addEventListener('mousedown', () => decrement(input));
 
-    input.addEventListener('change', () => {
-      onChangeDebounced(input);
+    input.addEventListener('focus', () => {
+      const inputValue = getInputValue(input);
+      input.prevValue = inputValue;
+      if (inputValue === 0) {
+        input.value = '';
+      }
     });
 
-    input.addEventListener('keydown', () => {
-      if (allowedKeys.includes(evt.key)) {
+    input.addEventListener('blur', () => {
+      if (input.value === '') {
+        input.value = 0;
+      }
+    });
+
+    input.addEventListener('input', (event) => {
+      handleInputChange(event);
+    });
+
+    input.addEventListener('change', (event) => {
+      handleInputChange(event);
+    });
+
+    input.addEventListener('keydown', (event) => {
+      if (allowedKeys.includes(event.key)) {
         return true;
       }
-      evt.preventDefault();
+      event.preventDefault();
     });
   });
 });
