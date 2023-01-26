7e3309090afc9ed3131b192182575f518996783d
sastmyhero <stasmyhero@gmail.com>
2023-01-27 02:20:21 +0300

    tabs

---
+++ src/js/components/tabs.js
@@ -0,0 +1,26 @@
+const activators = document.querySelectorAll('.tabs__activator');
+const items = document.querySelectorAll('.tabs__item');
+
+activators.forEach((activator) => {
+  activator.addEventListener('click', (event) => {
+    const tabId = event.currentTarget.dataset.tab;
+
+    const tabItem = document.querySelector(`.tabs__item[data-tab="${tabId}"]`);
+
+    activators.forEach((activator) => {
+      activator.classList.remove('tabs__activator_active');
+    });
+
+    event.target.classList.add('tabs__activator_active');
+
+    items.forEach((content) => {
+      content.classList.remove('tabs__item_active');
+    });
+
+    tabItem.classList.add('tabs__item_visible');
+    requestAnimationFrame(() => {
+      tabItem.classList.add('tabs__item_active');
+      tabItem.classList.remove('tabs__item_visible');
+    });
+  });
+});
