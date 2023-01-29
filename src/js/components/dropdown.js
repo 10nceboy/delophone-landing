3a2413e5366a9ed7e30112c825769bad77142699
10nce <10nceboy@gmail.com>
2023-01-29 16:37:53 +0400

    fix hero

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -1,30 +1,27 @@
 import { clickOutside } from '../utils/dom';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const dropdowns = document.querySelectorAll('.dropdown');
+const dropdowns = document.querySelectorAll('.dropdown');
 
-  const setDropdownValue = (dropdown, value) => {
-    const button = dropdown.querySelector('.dropdown__button');
-    dropdown.dataset.value = value;
-    button.innerText = value;
-  };
+const setDropdownValue = (dropdown, value) => {
+  const button = dropdown.querySelector('.dropdown__button');
+  dropdown.dataset.value = value;
+  button.innerText = value;
+};
 
-  dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener('click', () =>
-      dropdown.classList.toggle('active')
-    );
-
-    const items = dropdown.querySelectorAll('.dropdown__menu li');
-
-    /** handle item click => dispatch syntetic event  */
-    items.forEach((dropdownItem) => {
-      dropdownItem.addEventListener('click', () => {
-        if (dropdown.dataset.value) {
-          setDropdownValue(dropdown, dropdownItem.dataset.value);
-        }
-      });
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('dropdown_active');
+    requestAnimationFrame(() => dropdown.classList.toggle('dropdown_visible'));
+  });
+  const items = dropdown.querySelectorAll('.dropdown__menu li');
+  items.forEach((dropdownItem) => {
+    dropdownItem.addEventListener('click', () => {
+      if (dropdown.dataset.value) {
+        setDropdownValue(dropdown, dropdownItem.dataset.value);
+      }
     });
-
-    clickOutside(dropdown, () => dropdown.classList.remove('active'));
+  });
+  clickOutside(dropdown, () => {
+    dropdown.classList.remove('dropdown_active', 'dropdown_visible');
   });
 });
