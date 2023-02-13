8f1245d8481bdc5b210e8612213e67be57d397a7
10nce <10nceboy@gmail.com>
2023-02-13 14:01:29 +0400

    html parts

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'pages/full.html')
+        nested: resolve(__dirname, 'pages/price.html')
       }
     }
   }
