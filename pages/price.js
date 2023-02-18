1cde8fcd65c25fdb8e8a9432b5fb7cfe27659a30
10nce <10nceboy@gmail.com>
2023-02-18 18:14:25 +0400

    final fixes to price

---
+++ pages/price.js
@@ -0,0 +1,8 @@
+import '~/js/components/burger';
+import '~/js/components/expand.js';
+import '~/js/components/dropdown';
+import '~/js/components/collapse';
+import '~/js/components/modal';
+import '~/js/components/tooltip';
+import '~/js/components/city';
+import '~/js/components/menu';
