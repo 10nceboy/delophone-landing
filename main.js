c5e5c32e9ae49e243035b73d890462e14d399a3b
10nce <10nceboy@gmail.com>
2023-03-06 22:54:28 +0400

    autocomplete

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ import '~/js/components/collapse';
 import '~/js/components/modal';
 import '~/js/components/calculator';
 import '~/js/components/tabs';
-import '~/js/components/carousel';
+import '~/js/components/gift';
 import '~/js/components/tooltip';
 import '~/js/components/hero';
 import '~/js/components/city';
 import '~/js/components/menu';
 import '~/js/components/autocomplete';
+
