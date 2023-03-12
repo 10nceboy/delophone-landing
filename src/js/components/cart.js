68c6ef9826614052acfa3e71669b5c5a7f6330e6
10nce <10nceboy@gmail.com>
2023-03-12 15:35:57 +0400

    cart

--- src/js/components/cart.js
+++ src/js/components/cart.js
@@ -1,152 +1,131 @@
+import { phones } from "../constants/cart";
 
-
-import { numberQuantity, choice } from '../constants/cart'
 
 const cart = document.querySelector('.cart')
 const cartEl = document.querySelector('.cart__content-upside');
+const phoneCards = document.querySelector('.choose__phones-cards')
+let cartArray = []
 
-const numbers = document.querySelectorAll('.choose__phones-card')
 
 
-const renderItemHeader = () => {
-  const cartConHeader = document.createElement('div');
-  cartConHeader.className = 'cart__content-header';
-  cartEl.appendChild(cartConHeader);
-  const activeTab = document.querySelector('.tabs__activator_active');
-  const tabId = activeTab.dataset.tab;
-  const autocomplete = document.querySelector('.autocomplete__input')
-  if (tabId != "cities") {
-    cartConHeader.textContent = activeTab.textContent;
+
+phones.forEach(phone => {
+  switch (phone.type) {
+    case "single":
+      const card = document.createElement('div');
+      phoneCards.appendChild(card)
+      card.innerHTML = `<div class="card card_white choose__phones-card" data-id="${phone.id}">
+    <div class="choose__phones-single-number no-select">
+      <span class="choose__city-code" data-city-code="${phone.code1}"
+        >${phone.code1}</span> <span class="choose__phone-number no-select" data-number="${phone.number}"
+      >&nbsp;<i>${phone.number.charAt(0)}</i><i>${phone.number.charAt(1)}</i><i>${phone.number.charAt(2)}</i><i>-</i><i>${phone.number.charAt(3)}</i><i>${phone.number.charAt(4)}</i
+      ><i>-</i><i>${phone.number.charAt(5)}</i><i>${phone.number.charAt(6)}</i></span>
+    </div >
+  </div> `
+      break
+
+
+    case "pair":
+      const cardpair = document.createElement('div');
+      phoneCards.appendChild(cardpair)
+      cardpair.innerHTML = `<div class="card card_white choose__phones-card" data-id=${phone.id}>
+        <div class="choose__phones-pair-numbers">
+          <span class="no-select">
+            <span class="choose__city-code no-select"  data-city-code="${phone.code1}"
+            >${phone.code1}</span
+            >
+            <span class="choose__phone-number "
+            <i>${phone.number.charAt(0)}</i><i>${phone.number.charAt(1)}</i><i>${phone.number.charAt(2)}</i><i>-</i><i>${phone.number.charAt(3)}</i><i>${phone.number.charAt(4)}</i
+            ><i>-</i><i>${phone.number.charAt(5)}</i><i>${phone.number.charAt(6)}</i>
+            </span
+          ></span>
+          <div class="choose__phones-pair-complect">
+            <span class="no-select">Комплект 495+499</span
+            ><span
+              class="tooltip"
+              data-tooltip-width-laptop="300px"
+              data-tooltip-width-tablet="300px"
+              data-tooltip-width-smartphone="252px"
+              data-tooltip="Сразу два номера в комплекте: 
+          8 <i>${phone.code1.substring(2, 5)}</i> ${phone.number.substring(0, 3)}-${phone.number.substring(3, 5)}-${phone.number.substring(5, 7)}
+          8 <i>${phone.code2.substring(2, 5)}</i> ${phone.number.substring(0, 3)}-${phone.number.substring(3, 5)}-${phone.number.substring(5, 7)}
+          Номера продаются только вместе"
+            ></span>
+          </div>
+        </div>
+      `
+      break
+
+    case "vip":
+      const cardvip = document.createElement('div');
+      phoneCards.appendChild(cardvip)
+      cardvip.innerHTML =
+        `<div class="card card_white choose__phones-card" data-id ="${phone.id}" >
+        <div class="choose__phones-vip-number no-select">
+          <span class="choose__city-code">${phone.code1}</span>  <span class="choose__phone-number ">
+            <i>${phone.number.charAt(0)}</i><i>${phone.number.charAt(1)}</i><i>${phone.number.charAt(2)}</i><i>-</i><i>${phone.number.charAt(3)}</i><i>${phone.number.charAt(4)}</i
+            ><i>-</i><i>${phone.number.charAt(5)}</i><i>${phone.number.charAt(6)}</i>
+            </span>
+          <div class="choose__phones-vip-price" data-price=${phone.price}>${phone.price}</div>
+      </div>
+      </div>`
+      break
   }
-  else {
-    cartConHeader.textContent = autocomplete.value
-  }
+})
+
+
+const cards = document.querySelectorAll('.choose__phones-card')
+
+cards.forEach((card) => {
+  card.addEventListener('click', (event) => {
+    const id = event.target.dataset.id;
+    console.log(id)
+  })
 }
+)
 
 
-const renderItem = () => {
-  const cartItem = document.createElement('div');
-  cartEl.appendChild(cartItem);
-  cartItem.className = "card card_white cart__item";
-  cartItem.innerHTML = `<div class="cart__item-number">
-  <span class="choose__city-code">8 495</span> 243-45-61
-</div>
-<div class="cart__item-price">
-  990₽ разово,
-  <div class="cart__item-price_per-month">190₽/мес</div>
-</div>
-${renderDeleteIcon()}`
-}
-
-
-
-
-
-
-
-
-const renderDeleteIcon = () => {
-  return `
-    <svg
-    class="cart__item-icon"
-    width="25"
-    height="25"
-    viewBox="0 0 25 25"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <circle cx="12.5" cy="12.5" r="12" stroke="#9CA5AC" />
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M9.38201 8.6741C9.18674 8.47884 8.87016 8.47884 8.6749 8.6741C8.47964 8.86936 8.47964 9.18594 8.6749 9.38121L11.7941 12.5004L8.67489 15.6195C8.47963 15.8148 8.47963 16.1314 8.67489 16.3266C8.87016 16.5219 9.18674 16.5219 9.382 16.3266L12.5012 13.2075L15.62 16.3263C15.8153 16.5216 16.1319 16.5216 16.3271 16.3263C16.5224 16.1311 16.5224 15.8145 16.3271 15.6192L13.2083 12.5004L16.3271 9.38149C16.5224 9.18623 16.5224 8.86965 16.3271 8.67439C16.1319 8.47912 15.8153 8.47912 15.62 8.67439L12.5012 11.7933L9.38201 8.6741Z"
-      fill="#9CA5AC"
-    />
-  </svg>`;
-};
-
-
-
-numbers.forEach(number => (number.addEventListener(`click`, () => {
-  renderItemHeader();
-  renderItem()
-  const cartItem = document.querySelector('.cart__item')
-  document.querySelector('.cart__item-icon').addEventListener('click', () => { cartItem.remove() })
-})))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const buyButton = document.querySelector('.cart__button-buy')
-const cartQuantity = document.querySelector('.cart__quantity')
-const QuantityNumber = document.querySelector('.cart__quantity-number')
-const choiceHeader = document.querySelector('.cart__header-choice')
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-
-
-
-
-  let quantity = parseInt(cartQuantity.textContent.split(" ").join(""), 10)
-  const declination = (number, txt, cases = [2, 0, 1, 1, 1, 2]) => txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-
-
-
-
-
-  if (quantity == 0) {
-    buyButton.disabled = true;
-    choiceHeader.textContent = choice + 'о'
-    QuantityNumber.textContent = numberQuantity + 'ов'
+const addToCart = (id) => {
+  if (cart.some(item => item.id === id)) {
 
   }
   else {
-    if (buyButton) {
-      buyButton.disabled = false;
-    }
-    QuantityNumber.textContent = numberQuantity + declination(quantity, ['', 'а', 'ов'])
-    if (quantity % 2 == 1) {
-      choiceHeader.textContent = choice
-    }
-    else {
-      choiceHeader.textContent = choice + 'о'
-    }
-
-    if (quantity == 1) buyButton.textContent = "Купите номер телефона"
+    const item = phones.find(phone => phone.id === id)
+    cartArray.push(item)
   }
 
-});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/js/constants/cart.js
+++ src/js/constants/cart.js
@@ -9,4 +9,50 @@ export const codes = {
 };
 
 
-
+export const phones = [
+    {
+        id: "1",
+        type: "single",
+        filter: "city",
+        code1: '8 495',
+        code2: '',
+        number: '2434561',
+        price: "990₽ разово",
+    },
+    {
+        id: "2",
+        type: "pair",
+        filter: "city",
+        code1: "8 495",
+        code2: "8 499",
+        number: "2564561",
+        price: "1 990₽ разово",
+    },
+    {
+        id: "3",
+        type: "vip",
+        filter: "mobile",
+        code1: '8 495',
+        code2: '',
+        number: '7777777',
+        price: "450 000 ₽",
+    },
+    {
+        id: "4",
+        type: "vip",
+        filter: "mobile",
+        code1: '8 495',
+        code2: '',
+        number: '7777777',
+        price: "450 000 ₽",
+    },
+    {
+        id: "5",
+        type: "pair",
+        filter: "mobile",
+        code1: '8 495',
+        code2: '',
+        number: '7377777',
+        price: "150 000 ₽",
+    },
+];
