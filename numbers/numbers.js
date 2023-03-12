f579a1554d7b0de5b37fed260c7b0cf3a4e1a438
10nce <10nceboy@gmail.com>
2023-03-12 09:22:06 +0400

    fix html-5

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -1,5 +1,13 @@
+import '~/js/components/burger';
+import '~/js/components/dropdown';
+import '~/js/components/collapse';
+import '~/js/components/tabs';
+import '~/js/components/tooltip';
+import '~/js/components/city';
+import '~/js/components/menu';
 import '~/js/components/filter';
 import '~/js/components/search';
 import '~/js/components/cart';
 import '~/js/components/autocomplete';
 import '~/js/components/gift';
+
