a78753a63c224245c24accbd59ced4db5c0b5f9b
10nce <10nceboy@gmail.com>
2023-03-13 20:34:33 +0400

    add toggle gift on click to gift button

--- src/js/components/gift.js
+++ src/js/components/gift.js
@@ -1,19 +1,15 @@
-import { clickOutside } from "../utils/dom";
-
+import { clickOutside } from '../utils/dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const gift = document.querySelector('.gift')
-    const giftButton = document.querySelector('.gift__button')
-    const giftCloseButton = document.querySelector('.gift__close-button')
+  const gift = document.querySelector('.gift');
+  const giftButton = document.querySelector('.gift__button');
+  const giftCloseButton = document.querySelector('.gift__close-button');
 
+  giftButton.addEventListener('click', () => {
+    gift.classList.toggle('gift_active');
+  });
 
-    giftButton.addEventListener('click', () => {
-        gift.classList.add('gift_active')
-    }
-    )
-
-    giftCloseButton.addEventListener('click', () => {
-        gift.classList.remove('gift_active')
-    }
-    )
+  giftCloseButton.addEventListener('click', () => {
+    gift.classList.remove('gift_active');
+  });
 });
