8fb2096a79977d64403375d8e0e7d3843eacb7f6
10nce <10nceboy@gmail.com>
2023-04-26 18:28:52 +0400

    fix tooltip bug-3

--- vite.config.js
+++ vite.config.js
@@ -41,8 +41,7 @@ export default defineConfig({
         office: resolve(__dirname, 'pages/scope/office.html'),
         phone: resolve(__dirname, 'pages/scope/phone-replace.html'),
         away: resolve(__dirname, 'pages/scope/away.html'),
-        automatic: resolve(__dirname, 'pages/business/automatic.html'),
-        voicemenu: resolve(__dirname, 'pages/business/voice-menu.html')
+        automatic: resolve(__dirname, 'pages/business/automatic.html')
       }
     }
   }
