17d1129224aad22fc7aa3da71e07dd0917482d08
10nce <10nceboy@gmail.com>
2023-04-30 17:41:42 +0400

    audioplayer

--- src/js/components/audioplayer.js
+++ src/js/components/audioplayer.js
@@ -3,7 +3,14 @@ import { mutedIcon, pause, play, unMutedIcon } from '../constants/audioplayer';
 const volumes = document.querySelectorAll('.audioplayer__volume');
 const playButtons = document.querySelectorAll('.audioplayer__play');
 
-const handlePlayPause = (button) => {
+document.addEventListener('DOMContentLoaded', () => {
+  const audios = document.querySelectorAll('audio');
+  audios.forEach((audio) => {
+    audio.load();
+  });
+});
+
+const handlePlayToggle = (button) => {
   let isPlay = button.dataset.play;
   const audio = button.closest('.audioplayer').querySelector('audio');
   if (isPlay == 'false') {
@@ -17,31 +24,9 @@ const handlePlayPause = (button) => {
   }
 };
 
-playButtons.forEach((playPauseButton) => {
-  const playingAudio = playPauseButton
-    .closest('.audioplayer')
-    .querySelector('audio');
-
-  playingAudio.addEventListener('ended', () => {
-    handlePlayPause(playPauseButton);
-  });
-
-  playPauseButton.addEventListener('click', (event) => {
-    handlePlayPause(playPauseButton);
-    const toggledButtons = document.querySelectorAll(
-      '.audioplayer__play[data-play="true"]'
-    );
-
-    toggledButtons.forEach((toggledButton) => {
-      if (toggledButton !== event.currentTarget) {
-        handlePlayPause(toggledButton);
-      }
-    });
-  });
-});
-
-const handleMuteUnmute = (event) => {
+const handleMuteToggle = (event) => {
   let { target } = event;
+
   const audio = target.closest('.audioplayer').querySelector('audio');
   if (target.dataset.volume == 'unmuted') {
     target.innerHTML = mutedIcon;
@@ -54,6 +39,74 @@ const handleMuteUnmute = (event) => {
   }
 };
 
-volumes.forEach((volume) => {
-  volume.addEventListener('click', handleMuteUnmute);
+playButtons.forEach((playPauseButton) => {
+  const playingAudio = playPauseButton
+    .closest('.audioplayer')
+    .querySelector('audio');
+
+  playingAudio.addEventListener('ended', () => {
+    handlePlayToggle(playPauseButton);
+  });
+
+  playPauseButton.addEventListener('click', (event) => {
+    handlePlayToggle(playPauseButton);
+    const toggledButtons = document.querySelectorAll(
+      '.audioplayer__play[data-play="true"]'
+    );
+
+    toggledButtons.forEach((toggledButton) => {
+      if (toggledButton !== event.currentTarget) {
+        handlePlayToggle(toggledButton);
+      }
+    });
+  });
 });
+
+volumes.forEach((volume) => {
+  volume.addEventListener('click', handleMuteToggle);
+});
+
+const handleTimeLineClick = (event) => {
+  const { target: timeline, clientX } = event;
+  const audio = timeline.closest('.audioplayer').querySelector('.audio');
+  const rect = timeline.getBoundingClientRect();
+  const x = clientX - rect.left;
+  const pcent = Math.floor((x / rect.width) * 100);
+  const to = (audio.duration * pcent) / 100;
+  timeline.querySelector('.audioplayer__progress').style.width = `${pcent}%`;
+  audio.currentTime = to;
+  const button = timeline
+    .closest('.audioplayer')
+    .querySelector('.audioplayer__play');
+
+  const toggledButtons = document.querySelectorAll(
+    '.audioplayer__play[data-play="true"]'
+  );
+
+  toggledButtons.forEach((toggledButton) => {
+    if (toggledButton !== button) {
+      handlePlayToggle(toggledButton);
+    }
+  });
+};
+
+document.querySelectorAll('.audio').forEach((audio) => {
+  audio.addEventListener('timeupdate', () => {
+    const time = audio
+      .closest('.audioplayer')
+      ?.querySelector('.audioplayer__progress');
+    if (!time) return;
+    let perzent = (audio.currentTime / audio.duration) * 100;
+    time.style.width = `${perzent}%`;
+
+    if (perzent == 100) {
+      time.style.width = '0%';
+    }
+  });
+});
+
+document
+  .querySelectorAll('.audioplayer__progress-wrapper')
+  .forEach((timeline) => {
+    timeline.addEventListener('click', handleTimeLineClick);
+  });
