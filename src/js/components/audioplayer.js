303e9f5bc6692be101bd9d29f4679d9205eed6a5
10nce <10nceboy@gmail.com>
2023-05-01 14:14:09 +0400

    aduio7

--- src/js/components/audioplayer.js
+++ src/js/components/audioplayer.js
@@ -2,7 +2,7 @@ import { mutedIcon, pause, play, unMutedIcon } from '../constants/audioplayer';
 import { resetTimer, startTimer } from '../utils/timer';
 
 var audio1 = new Audio();
-audio1.src = '/public/audio/IVR-2.mp3';
+audio1.src = 'audio/IVR-2.mp3';
 
 var audioContext = new AudioContext();
 
