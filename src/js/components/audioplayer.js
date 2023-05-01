0dd4a24c8a55588d2b462e75960febc606842707
10nce <10nceboy@gmail.com>
2023-05-01 14:03:59 +0400

    aduio5

--- src/js/components/audioplayer.js
+++ src/js/components/audioplayer.js
@@ -1,22 +1,59 @@
 import { mutedIcon, pause, play, unMutedIcon } from '../constants/audioplayer';
 import { resetTimer, startTimer } from '../utils/timer';
 
+var audio1 = new Audio();
+audio1.src = '../../public/audio/IVR-2.mp3';
+
+var audioContext = new AudioContext();
+
+// Создаем новый объект SourceBuffer
+var sourceBuffer = audioContext.createBufferSource();
+
+// Создаем новый объект XMLHttpRequest для загрузки файла аудио
+var xhr = new XMLHttpRequest();
+xhr.open('GET', audio1.src, true);
+xhr.responseType = 'arraybuffer';
+
+xhr.onload = function () {
+  // Декодируем загруженный файл аудио и сохраняем его в объекте AudioBuffer
+  audioContext.decodeAudioData(xhr.response, function (buffer) {
+    // Устанавливаем буфер в SourceBuffer и начинаем воспроизведение
+    sourceBuffer.buffer = buffer;
+    sourceBuffer.connect(audioContext.destination);
+    sourceBuffer.start(0);
+  });
+};
+
+// Отправляем запрос на загрузку файла аудио
+xhr.send();
+
 const volumes = document.querySelectorAll('.audioplayer__volume');
 const playButtons = document.querySelectorAll('.audioplayer__play');
 
 const handlePlayToggle = (button) => {
   let isPlay = button.dataset.play;
-  const audio = button.closest('.audioplayer').querySelector('audio');
+
   if (isPlay == 'false') {
     button.innerHTML = pause;
     button.dataset.play = 'true';
-    audio.play();
+    audio1.play();
   } else {
     button.innerHTML = play;
     button.dataset.play = 'false';
-    audio.pause();
+    audio1.pause();
   }
 };
+
+audio1.addEventListener('play', function () {
+  // Воспроизводим аудио из буфера
+  sourceBuffer.start(0);
+});
+
+audio1.addEventListener('pause', function () {
+  // Останавливаем аудио и устанавливаем текущее время равным начальному времени
+  sourceBuffer.stop(0);
+  audio.currentTime = 0;
+});
 
 const handleMuteToggle = (event) => {
   let { target } = event;
